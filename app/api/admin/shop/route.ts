06682d7fe57b964b7abd96fd0533b5e443bd9082
u-mar <hazardcade44@gmail.com>
2025-12-30 05:11:20 +0800

    update

--- app/api/admin/shop/route.ts
+++ app/api/admin/shop/route.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/prisma/client";
+import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
